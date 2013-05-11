1fc83d559647187a49e98361f84aff86b6ea2896
G4br13l3 <gab.ponzi@stud.uniroma3.it>
2013-05-11 13:14:01 +0200

    Mess

---
+++ 2013-05-10/python/exercise1.py
@@ -0,0 +1,8 @@
+# EXERCISE 1
+
+
+
+
+
+# Tutte immagini di riferimento utilizzate per la modellazione di una BMW M3 Coupè e lo svolgimento 
+# degli esercizi proposti, si trovano come richiesto sotto il percorso 417229/2013-05-10/images/ .
